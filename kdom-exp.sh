e248fde95b38e1101aeb66450b9dc7cc75f005ae
Magnus Gedda <magnus.gedda@tomologic.com>
2017-03-09 17:58:04 +0100

    [kdom-exp.sh] Added git revisison to output and added subdirectory for output files.

--- kdom-exp.sh
+++ kdom-exp.sh
@@ -2,28 +2,24 @@
 
 set -e
 
-NUM_RUNS=1000
-PLAYER_STRATEGY="FULL_GREEDY"
+NUM_RUNS=1
+PLAYER_STRATEGY="TRUE_RANDOM"
 OPPONENT_STRATEGY="BASE_PLAYER"
 
 DATE_DAY=`date +%Y%m%d`
 DATE_TIME=`date +%H%M%S`
 DATE="$DATE_DAY-$DATE_TIME"
 
-OUTPUT_FILE="kdom-exp-${DATE}-[${PLAYER_STRATEGY}-vs-${OPPONENT_STRATEGY}].m"
-LOG_FILE="kdom-exp-${DATE}-[${PLAYER_STRATEGY}-vs-${OPPONENT_STRATEGY}].log"
+REVISION=$(eval "git rev-parse --short HEAD")
+
+TARGET_DIR="kdom_exp-${DATE}-rev-${REVISION}"
+
+OUTPUT_FILE="${TARGET_DIR}/kdom-exp-${DATE}-[${PLAYER_STRATEGY}-vs-${OPPONENT_STRATEGY}].m"
+LOG_FILE="${TARGET_DIR}/kdom-exp-${DATE}-[${PLAYER_STRATEGY}-vs-${OPPONENT_STRATEGY}].log"
+
 SCRIPT_STR="java -jar out/kdom-exp.jar ${PLAYER_STRATEGY} ${OPPONENT_STRATEGY} ${OUTPUT_FILE}"
 
-
-if [ -f $LOG_FILE ];
-then
-    rm $LOG_FILE
-fi
-
-if [ -f $OUTPUT_FILE ];
-then
-    rm $OUTPUT_FILE
-fi
+mkdir $TARGET_DIR
 
 
 function print
@@ -43,6 +39,7 @@ print "| Runs: $NUM_RUNS"
 print "| Output file: $OUTPUT_FILE"
 print "| "
 print "| Executing: '$SCRIPT_STR'"
+print "| Git Revision: $REVISION"
 print "---------------------------------------------------------------------------------------"
 print ""
 
@@ -57,6 +54,7 @@ echo "# Runs: $NUM_RUNS"                                                        
 echo "# Output file: $OUTPUT_FILE"                                                              >> $OUTPUT_FILE
 echo "# "                                                                                       >> $OUTPUT_FILE
 echo "# Executing: '$SCRIPT_STR'"                                                               >> $OUTPUT_FILE
+echo "# Git Revision: $REVISION"                                                                >> $OUTPUT_FILE
 echo "#--------------------------------------------------------------------------------------"  >> $OUTPUT_FILE
 echo ""                                                                                         >> $OUTPUT_FILE
 echo "# win, score, num_players, available_moves(13), available_draft(13), chosen_draft_position(13)" >> $OUTPUT_FILE
