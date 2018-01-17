2ac7547422343b83d1c8fe82285de12693f0c1f2
Magnus Gedda <github@gedda.se>
2018-01-17 10:52:00 +0100

    Update experiment scripts for new plots.

--- experiments/run_experiment_6.m
+++ experiments/run_experiment_6.m
@@ -37,84 +37,27 @@ mkdir(output_dir);
 
 opponent_strat_str = "FG";
 
-num_games = 20;
-time_limits = [0.5 2.0 5.0];        # time limits
-c_values = [0.1 0.2 0.3 0.4 0.5 1.0 1.5 2.0];   # UCB constant values
+num_games = 200;
+time_limits = [2.0];        # time limits
+c_values = [0.1 0.2 0.3 0.4 0.5];   # UCB constant values
 
-strat1_t0_5 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_1_W0_0_vs_FULL_GREEDY_T0.5_P0.dat', ' ', 19, 0);
-strat1_t2_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_1_W0_0_vs_FULL_GREEDY_T2_P0.dat', ' ', 19, 0);
-strat1_t5_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_1_W0_0_vs_FULL_GREEDY_T5_P0.dat', ' ', 19, 0);
-strat2_t0_5 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_2_W0_0_vs_FULL_GREEDY_T0.5_P0.dat', ' ', 19, 0);
-strat2_t2_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_2_W0_0_vs_FULL_GREEDY_T2_P0.dat', ' ', 19, 0);
-strat2_t5_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_2_W0_0_vs_FULL_GREEDY_T5_P0.dat', ' ', 19, 0);
-strat3_t0_5 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_3_W0_0_vs_FULL_GREEDY_T0.5_P0.dat', ' ', 19, 0);
-strat3_t2_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_3_W0_0_vs_FULL_GREEDY_T2_P0.dat', ' ', 19, 0);
-strat3_t5_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_3_W0_0_vs_FULL_GREEDY_T5_P0.dat', ' ', 19, 0);
-strat4_t0_5 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_4_W0_0_vs_FULL_GREEDY_T0.5_P0.dat', ' ', 19, 0);
-strat4_t2_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_4_W0_0_vs_FULL_GREEDY_T2_P0.dat', ' ', 19, 0);
-strat4_t5_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_4_W0_0_vs_FULL_GREEDY_T5_P0.dat', ' ', 19, 0);
-strat5_t0_5 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_5_W0_0_vs_FULL_GREEDY_T0.5_P0.dat', ' ', 19, 0);
-strat5_t2_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_5_W0_0_vs_FULL_GREEDY_T2_P0.dat', ' ', 19, 0);
-strat5_t5_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C0_5_W0_0_vs_FULL_GREEDY_T5_P0.dat', ' ', 19, 0);
-strat6_t0_5 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C1_0_W0_0_vs_FULL_GREEDY_T0.5_P0.dat', ' ', 19, 0);
-strat6_t2_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C1_0_W0_0_vs_FULL_GREEDY_T2_P0.dat', ' ', 19, 0);
-strat6_t5_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C1_0_W0_0_vs_FULL_GREEDY_T5_P0.dat', ' ', 19, 0);
-strat7_t0_5 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C1_5_W0_0_vs_FULL_GREEDY_T0.5_P0.dat', ' ', 19, 0);
-strat7_t2_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C1_5_W0_0_vs_FULL_GREEDY_T2_P0.dat', ' ', 19, 0);
-strat7_t5_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C1_5_W0_0_vs_FULL_GREEDY_T5_P0.dat', ' ', 19, 0);
-strat8_t0_5 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C2_0_W0_0_vs_FULL_GREEDY_T0.5_P0.dat', ' ', 19, 0);
-strat8_t2_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C2_0_W0_0_vs_FULL_GREEDY_T2_P0.dat', ' ', 19, 0);
-strat8_t5_0 = dlmread('kdom_exp-20180112-210350-rev-1b74839-cpu-2.70GHz/kdom_exp_UCT_TR_C2_0_W0_0_vs_FULL_GREEDY_T5_P0.dat', ' ', 19, 0);
+strat1_t2_0 = dlmread('kdom_exp-20180113-151620-rev-08e3392-cpu-3.20GHz/kdom_exp_UCT_TR_C0_1_W0_0_vs_FULL_GREEDY_T2_P0.dat', ' ', 19, 0);
+strat2_t2_0 = dlmread('kdom_exp-20180113-151620-rev-08e3392-cpu-3.20GHz/kdom_exp_UCT_TR_C0_2_W0_0_vs_FULL_GREEDY_T2_P0.dat', ' ', 19, 0);
+strat3_t2_0 = dlmread('kdom_exp-20180113-151620-rev-08e3392-cpu-3.20GHz/kdom_exp_UCT_TR_C0_3_W0_0_vs_FULL_GREEDY_T2_P0.dat', ' ', 19, 0);
+strat4_t2_0 = dlmread('kdom_exp-20180113-151620-rev-08e3392-cpu-3.20GHz/kdom_exp_UCT_TR_C0_4_W0_0_vs_FULL_GREEDY_T2_P0.dat', ' ', 19, 0);
+strat5_t2_0 = dlmread('kdom_exp-20180113-151620-rev-08e3392-cpu-3.20GHz/kdom_exp_UCT_TR_C0_5_W0_0_vs_FULL_GREEDY_T2_P0.dat', ' ', 19, 0);
 
-strat1_t0_5_str = "C=0.1 (0.5s)";
 strat1_t2_0_str = "C=0.1 (2s)";
-strat1_t5_0_str = "C=0.1 (5s)";
-strat2_t0_5_str = "C=0.2 (0.5s)";
 strat2_t2_0_str = "C=0.2 (2s)";
-strat2_t5_0_str = "C=0.2 (5s)";
-strat3_t0_5_str = "C=0.3 (0.5s)";
 strat3_t2_0_str = "C=0.3 (2s)";
-strat3_t5_0_str = "C=0.3 (5s)";
-strat4_t0_5_str = "C=0.4 (0.5s)";
 strat4_t2_0_str = "C=0.4 (2s)";
-strat4_t5_0_str = "C=0.4 (5s)";
-strat5_t0_5_str = "C=0.5 (0.5s)";
 strat5_t2_0_str = "C=0.5 (2s)";
-strat5_t5_0_str = "C=0.5 (5s)";
-strat6_t0_5_str = "C=1.0 (0.5s)";
-strat6_t2_0_str = "C=1.0 (2s)";
-strat6_t5_0_str = "C=1.0 (5s)";
-strat7_t0_5_str = "C=1.5 (0.5s)";
-strat7_t2_0_str = "C=1.5 (2s)";
-strat7_t5_0_str = "C=1.5 (5s)";
-strat8_t0_5_str = "C=2.0 (0.5s)";
-strat8_t2_0_str = "C=2.0 (2s)";
-strat8_t5_0_str = "C=2.0 (5s)";
 
-strats_1{1} = getStratCellArrayVersion4(strat1_t0_5, strat1_t0_5_str, opponent_strat_str);
-strats_1{2} = getStratCellArrayVersion4(strat1_t2_0, strat1_t2_0_str, opponent_strat_str);
-strats_1{3} = getStratCellArrayVersion4(strat1_t5_0, strat1_t5_0_str, opponent_strat_str);
-strats_2{1} = getStratCellArrayVersion4(strat2_t0_5, strat2_t0_5_str, opponent_strat_str);
-strats_2{2} = getStratCellArrayVersion4(strat2_t2_0, strat2_t2_0_str, opponent_strat_str);
-strats_2{3} = getStratCellArrayVersion4(strat2_t5_0, strat2_t5_0_str, opponent_strat_str);
-strats_3{1} = getStratCellArrayVersion4(strat3_t0_5, strat3_t0_5_str, opponent_strat_str);
-strats_3{2} = getStratCellArrayVersion4(strat3_t2_0, strat3_t2_0_str, opponent_strat_str);
-strats_3{3} = getStratCellArrayVersion4(strat3_t5_0, strat3_t5_0_str, opponent_strat_str);
-strats_4{1} = getStratCellArrayVersion4(strat4_t0_5, strat4_t0_5_str, opponent_strat_str);
-strats_4{2} = getStratCellArrayVersion4(strat4_t2_0, strat4_t2_0_str, opponent_strat_str);
-strats_4{3} = getStratCellArrayVersion4(strat4_t5_0, strat4_t5_0_str, opponent_strat_str);
-strats_5{1} = getStratCellArrayVersion4(strat5_t0_5, strat5_t0_5_str, opponent_strat_str);
-strats_5{2} = getStratCellArrayVersion4(strat5_t2_0, strat5_t2_0_str, opponent_strat_str);
-strats_5{3} = getStratCellArrayVersion4(strat5_t5_0, strat5_t5_0_str, opponent_strat_str);
-strats_6{1} = getStratCellArrayVersion4(strat6_t0_5, strat6_t0_5_str, opponent_strat_str);
-strats_6{2} = getStratCellArrayVersion4(strat6_t2_0, strat6_t2_0_str, opponent_strat_str);
-strats_6{3} = getStratCellArrayVersion4(strat6_t5_0, strat6_t5_0_str, opponent_strat_str);
-strats_7{1} = getStratCellArrayVersion4(strat7_t0_5, strat7_t0_5_str, opponent_strat_str);
-strats_7{2} = getStratCellArrayVersion4(strat7_t2_0, strat7_t2_0_str, opponent_strat_str);
-strats_7{3} = getStratCellArrayVersion4(strat7_t5_0, strat7_t5_0_str, opponent_strat_str);
-strats_8{1} = getStratCellArrayVersion4(strat8_t0_5, strat8_t0_5_str, opponent_strat_str);
-strats_8{2} = getStratCellArrayVersion4(strat8_t2_0, strat8_t2_0_str, opponent_strat_str);
-strats_8{3} = getStratCellArrayVersion4(strat8_t5_0, strat8_t5_0_str, opponent_strat_str);
+strats_1{1} = getStratCellArrayVersion4(strat1_t2_0, strat1_t2_0_str, opponent_strat_str);
+strats_2{1} = getStratCellArrayVersion4(strat2_t2_0, strat2_t2_0_str, opponent_strat_str);
+strats_3{1} = getStratCellArrayVersion4(strat3_t2_0, strat3_t2_0_str, opponent_strat_str);
+strats_4{1} = getStratCellArrayVersion4(strat4_t2_0, strat4_t2_0_str, opponent_strat_str);
+strats_5{1} = getStratCellArrayVersion4(strat5_t2_0, strat5_t2_0_str, opponent_strat_str);
 
 
 #----------------------------------
@@ -138,6 +81,21 @@ function score_diff_cell = getScoreDiffsCellArray(strats, strat_str)
   endfor
 
   score_diff_cell = {strat_str, avg_score_diff, lower_err, upper_err};
+endfunction
+
+function win_percentages_cell = getWinPercentagesCellArray(strats, strat_str)
+  win_percentages = [];
+
+  for i = 1:size(strats,2)
+    wins = strats{i}{6}(1);
+    draws = strats{i}{6}(2);
+    losses = strats{i}{6}(3);
+
+    win_percentage = (wins/(wins + draws + losses))*100;
+    win_percentages = [win_percentages  win_percentage];
+  endfor
+
+  win_percentages_cell = {strat_str, win_percentages};
 endfunction
 
 function plotScoreDiffs(score_diffs, num_games, x)
@@ -205,6 +163,21 @@ function writeScoreDiffsToDatFileInverted(score_diffs, time_values, x_values, ou
   endfor
 endfunction
 
+function writeWinPercentagesToDatFileInverted(win_percentages, time_values, x_values, output_dir)
+  for i = 1:size(time_values,2)  # loop over files
+    data = [];
+    for j = 1:size(win_percentages,2)
+      win_percentage = win_percentages{j}{2}(i);
+      data = [data; x_values(j) win_percentage];
+    endfor
+
+    t = time_values(i);
+    filename = [output_dir "/WIN_PERCENTAGES_TIME_" num2str(t) "s.dat"];
+
+    dlmwrite(filename, data, "delimiter", " ")
+  endfor
+endfunction
+
 
 #----------------------------------
 # Process experiment data.
@@ -215,12 +188,15 @@ score_diffs{2} = getScoreDiffsCellArray(strats_2, "C0.2");
 score_diffs{3} = getScoreDiffsCellArray(strats_3, "C0.3");
 score_diffs{4} = getScoreDiffsCellArray(strats_4, "C0.4");
 score_diffs{5} = getScoreDiffsCellArray(strats_5, "C0.5");
-score_diffs{6} = getScoreDiffsCellArray(strats_6, "C1.0");
-score_diffs{7} = getScoreDiffsCellArray(strats_7, "C1.5");
-score_diffs{8} = getScoreDiffsCellArray(strats_8, "C2.0");
 
 #plotScoreDiffs(score_diffs, num_games, time_limits);
 writeScoreDiffsToDatFile(score_diffs, output_dir, time_limits);
-
 writeScoreDiffsToDatFileInverted(score_diffs, time_limits, c_values, output_dir);
 
+win_percentages{1} = getWinPercentagesCellArray(strats_1, "C0.1");
+win_percentages{2} = getWinPercentagesCellArray(strats_2, "C0.2");
+win_percentages{3} = getWinPercentagesCellArray(strats_3, "C0.3");
+win_percentages{4} = getWinPercentagesCellArray(strats_4, "C0.4");
+win_percentages{5} = getWinPercentagesCellArray(strats_5, "C0.5");
+
+writeWinPercentagesToDatFileInverted(win_percentages, time_limits, c_values, output_dir);
