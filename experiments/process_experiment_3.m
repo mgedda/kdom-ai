80d5481e8311ff1f22171924a564d301c5789ceb
Magnus Gedda <github@gedda.se>
2018-02-08 00:11:38 +0100

    Update result processing script with new results for experiment 3.

--- experiments/process_experiment_3.m
+++ experiments/process_experiment_3.m
@@ -30,43 +30,41 @@ function process_experiment_3(target_path, output_dir)
     plot_functions;
 
     opponent_strat_str = "FG";
-    #num_games = 450;
-    num_games = 300;
+    num_games = 500;
 
     #
     # Read experiment result files
     #
 
-    source("runs/kdom_exp-20170326-190517-rev-fd90607/kdom_exp_FULL_GREEDY_vs_FULL_GREEDY.m");
-    source("runs/kdom_exp-20170326-005955-rev-fd90607/kdom_exp_MC_TR_TR_WDL_vs_FULL_GREEDY.m");
-    source("runs/kdom_exp-20170328-150600-rev-8f1e859/kdom_exp_MC_TR_TR_P_vs_FULL_GREEDY.m");
-    source("runs/kdom_exp-20170327-161148-rev-8f1e859/kdom_exp_MC_TR_TR_R_vs_FULL_GREEDY.m");
-    source("runs/kdom_exp-20170331-213055-rev-3cc76b2-cpu-3.20GHz/kdom_exp_MCE_FG_R_vs_FULL_GREEDY_T10_P0.m");
-    strat1_str = "FG";
-    strat2_str = "MCE-TR/WDL";
-    strat3_str = "MCE-TR/P";
-    strat4_str = "MCE-TR/R";
-    strat5_str = "MCE-FG/R";
-    strat1 = kdom_exp_FULL_GREEDY_vs_FULL_GREEDY(1:num_games,:);
-    strat2 = kdom_exp_MC_TR_TR_WDL_vs_FULL_GREEDY(1:num_games,:);
-    strat3 = kdom_exp_MC_TR_TR_P_vs_FULL_GREEDY(1:num_games,:);
-    strat4 = kdom_exp_MC_TR_TR_R_vs_FULL_GREEDY(1:num_games,:);
-    strat5 = kdom_exp_MCE_FG_R_vs_FULL_GREEDY_T10_P0(1:num_games,:);
+    strat1 = dlmread('runs/kdom_exp-20180205-150829-rev-85a55d8-cpu-3.20GHz/kdom_exp_MCE_TR_WDL_vs_FULL_GREEDY_G500_T5_P0.dat', ' ', 19, 0);
+    strat2 = dlmread('runs/kdom_exp-20180205-150829-rev-85a55d8-cpu-3.20GHz/kdom_exp_MCE_TR_P_vs_FULL_GREEDY_G500_T5_P0.dat', ' ', 19, 0);
+    strat3 = dlmread('runs/kdom_exp-20180205-150829-rev-85a55d8-cpu-3.20GHz/kdom_exp_MCE_TR_R_vs_FULL_GREEDY_G500_T5_P0.dat', ' ', 19, 0);
+
+    #
+    # Set run names
+    #
+
+    strat1_str = "MCE-TR/WDL";
+    strat2_str = "MCE-TR/P";
+    strat3_str = "MCE-TR/R";
+
+    #
+    # Create cell arrays
+    #
+
+    strats{1} = getStratCellArrayVersion4(strat1, strat1_str, opponent_strat_str);
+    strats{2} = getStratCellArrayVersion4(strat2, strat2_str, opponent_strat_str);
+    strats{3} = getStratCellArrayVersion4(strat3, strat3_str, opponent_strat_str);
+
 
     #
     # Process data
     #
 
-    strats{1} = getStratCellArrayVersion3(strat1, strat1_str, opponent_strat_str);
-    strats{2} = getStratCellArrayVersion3(strat2, strat2_str, opponent_strat_str);
-    strats{3} = getStratCellArrayVersion3(strat3, strat3_str, opponent_strat_str);
-    strats{4} = getStratCellArrayVersion3(strat4, strat4_str, opponent_strat_str);
-    strats{5} = getStratCellArrayVersion3(strat5, strat5_str, opponent_strat_str);
-
 
     # Hypothesis test.
     #
-    pval = t_test_2(strats{3}{5}, strats{2}{5}, ">")
+    pval = t_test_2(strats{2}{5}, strats{3}{5}, ">")
 
 
     # Win/Loss/Draw pie charts.
@@ -89,7 +87,7 @@ function process_experiment_3(target_path, output_dir)
 
     # Playouts per second
     #
-    plotPlayoutsPerSecondOverlap(strats(2:3));
+    plotPlayoutsPerSecondOverlap(strats(1:2));
     writePlayoutsToDatFile(strats, output_path, output_file_prefix);
 
 
