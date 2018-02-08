a878b71bfd120317c189c737fac01008e0bad62d
Magnus Gedda <github@gedda.se>
2018-02-09 00:07:33 +0100

    [process_results.sh] Disabled experiment_6_prel.

--- experiments/process_results.sh
+++ experiments/process_results.sh
@@ -21,7 +21,7 @@ octave --no-gui --norc --silent --eval "process_experiment_1('$TARGET_PATH', 'da
 octave --no-gui --norc --silent --eval "process_experiment_2('$TARGET_PATH', 'data_files/experiment_2')" > /dev/null 2>&1
 octave --no-gui --norc --silent --eval "process_experiment_3('$TARGET_PATH', 'data_files/experiment_3')" > /dev/null 2>&1
 octave --no-gui --norc --silent --eval "process_experiment_4('$TARGET_PATH', 'data_files/experiment_4')" > /dev/null 2>&1
-octave --no-gui --norc --silent --eval "process_experiment_6_prel('$TARGET_PATH', 'data_files/experiment_6_prel')" > /dev/null 2>&1
+#octave --no-gui --norc --silent --eval "process_experiment_6_prel('$TARGET_PATH', 'data_files/experiment_6_prel')" > /dev/null 2>&1
 octave --no-gui --norc --silent --eval "process_experiment_6('$TARGET_PATH', 'data_files/experiment_6')" > /dev/null 2>&1
 
 
@@ -31,7 +31,7 @@ gnuplot -e "target_path='$TARGET_PATH'; data_files_dir='data_files/experiment_1'
 gnuplot -e "target_path='$TARGET_PATH'; data_files_dir='data_files/experiment_2'" produce_plots_experiment_2.gp > /dev/null 2>&1
 gnuplot -e "target_path='$TARGET_PATH'; data_files_dir='data_files/experiment_3'" produce_plots_experiment_3.gp > /dev/null 2>&1
 gnuplot -e "target_path='$TARGET_PATH'; data_files_dir='data_files/experiment_4'" produce_plots_experiment_4.gp > /dev/null 2>&1
-gnuplot -e "target_path='$TARGET_PATH'; data_files_dir='data_files/experiment_6_prel'" produce_plots_experiment_6_prel.gp > /dev/null 2>&1
+#gnuplot -e "target_path='$TARGET_PATH'; data_files_dir='data_files/experiment_6_prel'" produce_plots_experiment_6_prel.gp > /dev/null 2>&1
 gnuplot -e "target_path='$TARGET_PATH'; data_files_dir='data_files/experiment_6'" produce_plots_experiment_6.gp > /dev/null 2>&1
 
 
